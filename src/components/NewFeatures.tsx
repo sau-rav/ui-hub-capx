1997e74443aeeaad6d45010a031f59c55d92d444
Saurav Kumar Sharma <39721874+sau-rav@users.noreply.github.com>
2024-04-23 00:08:16 +0530

    Update NewFeatures.tsx

--- src/components/NewFeatures.tsx
+++ src/components/NewFeatures.tsx
@@ -5,7 +5,7 @@ export const NewFeatures = (): JSX.Element => {
   return (
     <div className="bg-black text-white flex flex-col justify-center text-center py-36 bg-hero-image-2 bg-cover">
       <div className="text-3xl lg:text-5xl md:text-4xl"> SAY HELLO TO</div>
-      <div className="text-5xl lg:text-9xl md:text-7xl font-mono">Cap</div>
+      <div className="text-5xl lg:text-9xl md:text-7xl font-mono">Cap X</div>
       <div className="text-3xl lg:text-5xl md:text-4xl font-mono w-full px-20 pt-20">
         {DESCRIPTION}
       </div>

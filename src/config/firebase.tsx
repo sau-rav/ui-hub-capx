762d5b16d45693a03903626d563de474149cf351
sau-rav <saurav23aug@gmail.com>
2024-05-04 13:47:18 +0530

    changes for moving auth to capx.live account

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -5,18 +5,17 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKJJz4zuC7rJzGBCmN-ReNmTchYuV0wq4",
-  authDomain: "xyz-beta-a84c2.firebaseapp.com",
-  projectId: "xyz-beta-a84c2",
-  storageBucket: "xyz-beta-a84c2.appspot.com",
-  messagingSenderId: "217685714468",
-  appId: "1:217685714468:web:0b82016d4f576e225b9ab1",
-  measurementId: "G-80DCR55EH3"
+  apiKey: "AIzaSyAqa1ooFE07By9VR-VlK-5xIdHM7xO3JLc",
+  authDomain: "capx-live.firebaseapp.com",
+  projectId: "capx-live",
+  storageBucket: "capx-live.appspot.com",
+  messagingSenderId: "983794755941",
+  appId: "1:983794755941:web:9112d9997093afafd6198c",
+  measurementId: "G-795NJ46C9H"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export default app;

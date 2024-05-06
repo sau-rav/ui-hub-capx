b901c3a6dbaaa636d550c0a9c07d1f2b3eb0556b
sau-rav <saurav23aug@gmail.com>
2024-05-06 22:39:38 +0530

    stick navbar, logout message

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c1e404fbcfd1785d"; }
+/******/ 		__webpack_require__.h = function() { return "089ce45d5b7a006f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

a74c5fb3d14825c6ebf4e5a23ad52a0939467613
Rahul Verma <rahul.v@sprinklr.com>
2024-05-09 11:34:45 +0530

    gimpse bg image fix

--- src/components/Glimpse.tsx
+++ src/components/Glimpse.tsx
@@ -2,16 +2,16 @@ import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 
 export const Glimpse = (): JSX.Element => {
   return (
-    <div className="pt-16 lg:pt-32 flex flex-col bg-black gap-8 px-8">
-      <div className="text-white justify-center italic flex gap-8 text-4xl lg:text-8xl md:text-6xl font-montserrat">
+    <div className="pt-16 lg:pt-32 flex flex-col bg-black gap-8">
+      <div className="text-white justify-center italic flex gap-8 text-4xl lg:text-8xl md:text-6xl px-8">
         <p>
           <span className="text-golden">Sneak peek </span>
           <span>into what we are building</span>
         </p>
       </div>
-      <div className="p-px bg-gradient-to-r from-black via-golden via-50& to-black inline-block ">
+      <div className="p-px bg-gradient-to-r from-black via-golden via-50& to-black inline-block w-full">
         <div
-          className="bg-black w-full h-full p-52 flex justify-center bg-cover"
+          className="bg-black py-40 flex justify-center w-full"
           style={{
             backgroundImage: 'url("hero-3.png")',
             backgroundSize: "cover",

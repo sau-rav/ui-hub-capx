4e51e71b349b0347340febb63fb19ec6472fa14c
Rahul Verma <rahul.v@sprinklr.com>
2024-05-18 17:44:36 +0530

    Header

--- src/components/NewFeatures.tsx
+++ src/components/NewFeatures.tsx
@@ -8,15 +8,17 @@ export const NewFeatures = (): JSX.Element => {
   return (
     <div
       className="bg-black text-white flex flex-col justify-center items-center lg:py-48 py-12 md:py-24 bg-cover gap-4"
-      style={{
-        backgroundImage: 'url("hero-2.png")',
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
+      // style={{
+      //   backgroundImage: 'url("hero-2.png")',
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      // }}
     >
-      <Image src={logo} alt="Cap X" width={200} />
+      <div className="flex items-center justify-center text-2xl lg:text-6xl md:text-4xl font-montserrat text-center text-golden tracking-widest">
+        Change the way you trade
+      </div>
       <div
-        className="text-2xl lg:text-6xl md:text-4xl font-montserrat w-full pt-20 px-10 flex justify-center lg:w-1/2 md:w-3/4"
+        className="text-2xl lg:text-6xl md:text-4xl font-montserrat w-full pb-20 pt-10 px-10 flex justify-center lg:w-1/2 md:w-3/4 text-center"
         style={{ lineHeight: 1.25 }}
       >
         {DESCRIPTION}

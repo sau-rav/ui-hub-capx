a159fe25736e87bef96075ec9fc7171c4750d045
Parv Saxena <86960448+parv-FE-dev@users.noreply.github.com>
2024-05-20 23:57:02 +0530

    Updated footer component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 
-import { NavBar } from "../src/components/NavBar";
-import { Introduction } from "../src/components/introduction";
+import { Divider } from "../src/components/Divider";
+import { FeatureSlider } from "../src/components/featureSlider";
+import Footer from "../src/components/Footer/Footer";
 import { Glimpse } from "../src/components/Glimpse";
 import { Header } from "../src/components/Header";
-import { FeatureSlider } from "../src/components/featureSlider";
-import { RingBackdrop } from "../src/components/ringBackdrop";
-import { Divider } from "../src/components/Divider";
-import { NewFeatures } from "../src/components/NewFeatures";
+import { Introduction } from "../src/components/introduction";
+import { NavBar } from "../src/components/NavBar";
 import { NewFeatureCards } from "../src/components/newFeatureCards";
-import { Footer } from "../src/components/Footer";
+import { NewFeatures } from "../src/components/NewFeatures";
+import { RingBackdrop } from "../src/components/ringBackdrop";
 
 const Home: NextPage = () => {
   return (
--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = (): JSX.Element => {
-  return (
-    <div className="text-white flex w-full justify-center py-20">
-      CapX 2024. Made in India 🇮🇳
-    </div>
-  );
-};

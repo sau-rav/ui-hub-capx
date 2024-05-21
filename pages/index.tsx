59b9e01279785690a0a4b652bb59fece02fc656d
parv-FE-dev <parvsaxena94@gmail.com>
2024-05-21 23:14:04 +0530

    Footer changes updated.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,12 @@ import type { NextPage } from "next";
 import { Divider } from "../src/components/Divider";
 import { FeatureSlider } from "../src/components/featureSlider";
 import { Glimpse } from "../src/components/Glimpse";
+import Footer from "../src/components/footer";
 import { Header } from "../src/components/Header";
-import { Introduction } from "../src/components/introduction";
 import { NavBar } from "../src/components/NavBar";
 import { NewFeatureCards } from "../src/components/newFeatureCards";
-import { Footer } from "../src/components/Footer";
 import { TradersCount } from "../src/components/TradersCount";
 import { NewFeatures } from "../src/components/NewFeatures";
-import { RingBackdrop } from "../src/components/ringBackdrop";
 
 const Home: NextPage = () => {
   return (

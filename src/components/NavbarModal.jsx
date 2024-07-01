744b09a59fdfa6fabc4c8a04a3818b4901c229e4
Rahul Verma <rahul.v@sprinklr.com>
2024-07-01 23:22:46 +0530

    show join waitlist upfront

--- src/components/NavbarModal.jsx
+++ src/components/NavbarModal.jsx
@@ -13,7 +13,7 @@ import {NavModalStyle, boxStyle} from './Navbar.constants';
 import logo from "../../public/logo.png";
 
 
-export default function NavbarModal({ isModalOpen, handleModalClose, handleJoinWaitlist, handleHome }) {
+export default function NavbarModal({ isModalOpen, handleModalClose, handleLogout, handleHome }) {
 
     return (
         <div>
@@ -37,15 +37,15 @@ export default function NavbarModal({ isModalOpen, handleModalClose, handleJoinW
                             <Image src={logo} alt="logo" height={38} onClick={handleHome} />
                             <CloseIcon sx={{ fontSize: 30, color: "#FFFFFF" }} onClick={handleModalClose} />
                         </div>
-                        <div className="flex flex-col items-center" style={{ marginBottom: '20px', marginTop: '20px' }}>
+                        {/* <div className="flex flex-col items-center" style={{ marginBottom: '20px', marginTop: '20px' }}>
                             <Link  href="/faq" className="w-full font-semibold text-[24px] leading-[29px]" style={{ color: '#fff', paddingBottom: '10px', borderBottom: '1px solid #595959', zIndex: '999999' }} onClick={handleModalClose}>FAQ</Link>
-                        </div>
+                        </div> */}
                         <button
-                            className="h-full w-max md:px-6 py-2 md:py-3 rounded-full relative bg-golden text-black hover:font-bold"
+                            className="h-full w-max md:px-6 py-2 md:py-3 rounded-full relative bg-golden text-black hover:font-bold mt-4"
                             style={{paddingLeft: '34px', paddingRight: '34px'}}
-                            onClick={handleJoinWaitlist}
+                            onClick={handleLogout}
                         >
-                            <span>Join Waitlist</span>
+                            <span>Logout</span>
                         </button>
                     </Box>
                 </Fade>

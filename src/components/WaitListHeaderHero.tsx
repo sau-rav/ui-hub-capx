3fac1713220feef51302967d1c58aa761beaff90
Rahul Verma <rahul.v@sprinklr.com>
2024-07-21 23:03:19 +0530

    join waitlist header hero image

---
+++ src/components/WaitListHeaderHero.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+
+import { motion, useInView, useAnimation } from "framer-motion";
+
+import { useIsMobile } from "../hooks/useIsMobile";
+
+import waitListHero from "../../public/joinWaitListHero.png";
+
+export const WaitListHeaderHero = (): JSX.Element => {
+  const isMobile = useIsMobile();
+
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true });
+
+  const control = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("end");
+    }
+  }, [inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      className="overflow-hidden"
+      variants={{
+        start: { opacity: 0, x: 0, y: 412, scale: 2 },
+        end: { opacity: 1, x: 0, y: 0, scale: 1 },
+      }}
+      initial="start"
+      animate={control}
+      transition={{ duration: 0.5, delay: 0 }}
+    >
+      <Image
+        src={waitListHero}
+        alt="waitListHero"
+        height={isMobile ? 280 : 320}
+      />
+    </motion.div>
+  );
+};
--- src/components/WaitlistHeader.tsx
+++ src/components/WaitlistHeader.tsx
@@ -3,6 +3,8 @@ import { useRouter, NextRouter } from "next/router";
 import { signInWithPopup } from "firebase/auth";
 import Image from "next/image";
 
+import { WaitListHeaderHero } from "./WaitListHeaderHero";
+
 import { useUser } from "../context/user";
 import { useIsMobile } from "../hooks/useIsMobile";
 import { useSignUpMutation } from "../hooks/useSignUpMutation";
@@ -38,67 +40,43 @@ export const WaitlistHeader = (): JSX.Element => {
   }, [setUser]);
 
   return (
-    <div className={`flex ${isMobile ? "flex-col" : ""}`}>
-      <div
-        className={`flex-1 flex flex-col text-white gap-1 font-poppins gap-12 justify-center ${
-          isMobile
-            ? "pt-40 pb-0 pl-4 text-center items-center"
-            : "pt-0 pl-16 items-start"
-        }`}
-      >
-        <div className="flex flex-col gap-2">
-          <p className="text-3xl md:text-7xl font-bold">The next generation</p>
-          <p className="text-3xl md:text-7xl font-bold">trading platform</p>
-          <p className="text-xl md:text-3xl font-semibold text-golden-light  mt-8 md:mt-12">
-            Join now for Early Access
-          </p>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <div className="flex">
-            <button
-              className="h-full px-6 py-3 rounded-xl relative bg-white text-black absolute flex hover:bg-golden"
-              onClick={signInWithGoogle}
-            >
-              <img
-                src="https://p1.edge.duggup.com/tpt-icons/google.svg"
-                alt="Google Logo"
-                className="lg:w-8 h-8 lg:mr-2"
-              />
-              <span className="text-light-grey hover:text-black text-lg md:text-xl">
-                Signup using Google
-              </span>
-            </button>
-          </div>
-          <p className="text-md md:text-xl text-white text-left">
-            Signup using your google account
-          </p>
-        </div>
+    <div
+      className={`flex-1 flex flex-col text-white gap-4 font-poppins gap-4 justify-center h-screen items-center justify-center text-center max-h-screen overflow-hidden ${
+        isMobile ? "pt-40 pb-0 pl-4" : "pt-0 pl-16"
+      }`}
+    >
+      <div className="flex flex-col gap-2 md:mt-24">
+        <p className="text-3xl md:text-5xl font-extrabold">
+          The next generation
+        </p>
+        <p className="text-3xl md:text-5xl font-extrabold">trading platform</p>
+        <p className="text-xl md:text-3xl font-semibold text-golden-light  mt-2 ">
+          Join now for Early Access
+        </p>
       </div>
-      {isMobile ? (
-        <div className="h-96">
-          <div
-            className="flex-1 h-full"
-            style={{
-              backgroundImage: `url('joinWaitlist.png')`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          ></div>
+
+      <div className="flex flex-col gap-4 items-center justify-center group mb-10">
+        <div className="flex">
+          <button
+            className="h-full px-3 py-3 rounded-full relative border text-black absolute flex group-hover:bg-golden-light group-hover:border-golden"
+            onClick={signInWithGoogle}
+          >
+            <img
+              src="https://p1.edge.duggup.com/tpt-icons/google.svg"
+              alt="Google Logo"
+              className="lg:w-8 h-8 lg:mr-2"
+            />
+            <span className="text-white group-hover:text-black text-lg md:text-xl">
+              Signup using Google
+            </span>
+          </button>
         </div>
-      ) : (
-        <div className={`flex-1 justify-center items-center w-1/2 h-1/2`}>
-          <Image
-            src={joinWaitlist}
-            alt="coin"
-            width={0}
-            height={0}
-            style={{ width: "100%", height: "auto" }}
-            objectFit="cover"
-          ></Image>
-        </div>
-      )}
+        <p className="text-md md:text-xl text-white text-left">
+          Signup using your google account
+        </p>
+      </div>
+
+      <WaitListHeaderHero />
     </div>
   );
 };

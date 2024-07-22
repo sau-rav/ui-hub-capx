06d53c3689680cae363fb1b98bc41f6f90765301
Rahul Verma <rahul.v@sprinklr.com>
2024-07-22 10:07:09 +0530

    profile page

--- src/components/NavbarModal.jsx
+++ src/components/NavbarModal.jsx
@@ -1,3 +1,6 @@
+import { useRouter , NextRouter} from "next/router";
+import { useCallback } from "react";
+
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
@@ -11,8 +14,13 @@ import {NavModalStyle, boxStyle} from './Navbar.constants';
 
 import logo from "../../public/logo.png";
 
+const ITEM_CLASS_NAME = "cursor-pointer w-full font-semibold text-[24px] leading-[29px]"
+const ITEM_STYLES = { color: '#fff', paddingBottom: '10px', borderBottom: '1px solid #595959', zIndex: '999999' }
 
 export default function NavbarModal({ isModalOpen, handleModalClose, handleLogout, handleHome }) {
+    const router = useRouter();
+
+    const onProfileClick = useCallback(() => {router.push("/profile")}, [router]);
 
     return (
         <div>
@@ -36,8 +44,13 @@ export default function NavbarModal({ isModalOpen, handleModalClose, handleLogou
                             <Image src={logo} alt="logo" height={38} onClick={handleHome} />
                             <CloseIcon sx={{ fontSize: 30, color: "#FFFFFF" }} onClick={handleModalClose} />
                         </div>
+                        
                         <div className="flex flex-col items-center" style={{ marginBottom: '20px', marginTop: '20px' }}>
-                            <div onClick={handleLogout}className="cursor-pointer w-full font-semibold text-[24px] leading-[29px]" style={{ color: '#fff', paddingBottom: '10px', borderBottom: '1px solid #595959', zIndex: '999999' }} >Logout</div>
+                            <div onClick={onProfileClick} className={ITEM_CLASS_NAME} style={ITEM_STYLES} >Profile</div>
+                        </div>
+
+                        <div className="flex flex-col items-center" style={{ marginBottom: '20px', marginTop: '20px' }}>
+                            <div onClick={handleLogout}className={ITEM_CLASS_NAME} style={ITEM_STYLES} >Logout</div>
                         </div>
 
                     </Box>

24c7caeda05efd1f503b3700a3c1691d0a67b229
Rahul Verma <rahul.v@sprinklr.com>
2024-07-29 22:45:48 +0530

    thankyou page modal

---
+++ src/constants/thankyouModalStyles.ts
@@ -0,0 +1,17 @@
+export const THANKYOU_MODAL_STYLES = {
+  background:
+    "linear-gradient(109deg, rgba(51, 50, 50, 0.26) 10.11%, rgba(51, 50, 50, 0.07) 101.56%)",
+  backdropFilter: "blur(17px)",
+  WebkitBackdropFilter: "blur(17px)",
+};
+
+export const CONTAINER_STYLES = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  border: "1px solid #4F4F4F",
+  boxShadow: 24,
+  borderRadius: "16px",
+};

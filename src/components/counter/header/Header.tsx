7fb83b0d67d40f092092b38b586e5f282f509de8
Rahul Verma <rahul.v@sprinklr.com>
2024-08-06 00:05:27 +0530

    orange bg

--- src/components/counter/header/Header.tsx
+++ src/components/counter/header/Header.tsx
@@ -42,7 +42,7 @@ export const Header = (): JSX.Element => {
       className={`flex flex-col text-white gap-1 font-poppins text-center relative ${
         isMobile ? "px-8 pt-36" : "px-20 justify-center h-screen"
       }`}
-      style={{ height: isMobile ? "100vh" : "100vh" }}
+      style={{ height: "100vh" }}
     >
       <div
         className="absolute"
@@ -58,7 +58,7 @@ export const Header = (): JSX.Element => {
 
       <LineGolden />
 
-      {isMobile ? null : <Card1 />}
+      <Card1 />
 
       <Card2 />
 
@@ -66,14 +66,14 @@ export const Header = (): JSX.Element => {
 
       <p className="w-fit text-3xl md:text-7xl font-semibold">An AI-powered</p>
       <p className="w-fit text-3xl md:text-7xl font-semibold text-left">
-        trading platform that
+        investing platform that
       </p>
       <p className="w-fit text-3xl md:text-7xl font-semibold">works as your</p>
       <p className="w-fit text-3xl md:text-7xl font-semibold ">
         personal assistant
       </p>
       <p className="w-fit text-xl md:text-2xl font-normal text-golden-light text-left md:text-center mt-2 md:mt-12">
-        Research to investing, all-in-one place
+        All your investing needs, in one place
       </p>
     </div>
   );
